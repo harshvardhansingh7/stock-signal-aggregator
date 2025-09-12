6dda588bae1f3ead762011ed3d8779eb73c6bc16
harshcodeoftenly <harshvardhan1483@gmail.com>
2025-09-12 13:39:57 +0530

    Secure API key using .env, fixed OpenRouterService dependency injection

--- src/main/java/com/stockapp/StockAggregator/StockAggregatorApplication.java
+++ src/main/java/com/stockapp/StockAggregator/StockAggregatorApplication.java
@@ -1,5 +1,6 @@
 package com.stockapp.StockAggregator;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockAggregatorApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("OPENROUTER_API_KEY", dotenv.get("OPENROUTER_API_KEY"));
 		SpringApplication.run(StockAggregatorApplication.class, args);
 	}
 
